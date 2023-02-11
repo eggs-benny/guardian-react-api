542881713882f99c3c2b1c5882b33bf2625a0941
eggs-benny <benedict.smith87@gmail.com>
2023-02-11 11:50:05 +0000

    updated with initial styling & naming

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <div>
+  <h1><span className="highlight">The</span>Guardian</h1>
+  <div class="App">
+    Add a Search Bar
+    <div className="Feed">
+     Add the feed, which houses articles
+    </div>
+  </div>
+</div>
+    </div>
+  );
+}
+
+export default App;
